6f8e437aa38db0bb4c5a7c47f392302f819963e8
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-11 13:30:45 +0530

    validate_3

--- Registration_Page/script.js
+++ Registration_Page/script.js
@@ -1,2 +1,28 @@
 const form = document.querySelector("form")
 allInput = form.querySelectorAll(".first input");
+
+// Get the full name input field
+var fullNameInput = document.querySelector('input[type="text"][placeholder="Enter Your Name"]');
+
+// Add an event listener to the input field
+fullNameInput.addEventListener('input', function() {
+  // Get the current value of the input field
+  var fullName = fullNameInput.value;
+
+  // Check if the full name meets the validation criteria
+  var isValid = /^[A-Za-z\s]{5,12}$/.test(fullName) && !/\d/.test(fullName);
+
+  // Display an error message if the validation fails
+  if (!isValid) {
+    fullNameInput.setCustomValidity('Please enter a valid full name without numbers (5-12 characters)');
+  } else {
+    fullNameInput.setCustomValidity('');
+  }
+});
+
+
+
+
+
+
+
